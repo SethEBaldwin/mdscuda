750172b86cac5d2f08ed41423cdc7fbf91d66e59
SethEBaldwin <seth.e.baldwin@gmail.com>
2021-04-04 07:43:13 -0400

    add example khan

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     packages = find_packages(),
     install_requires=[
         'numpy>=1.19.0',
-        'pandas>=1.0.0',
         'scipy>=1.6.0',
-        'numba>=53.0'
+        'numba>=0.51.0'
     ]
 )
